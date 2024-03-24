dd652240d446d3275f5d8ca5e59f2b9a4853ccf8
bjacobsUNB <b.jacobs@unb.ca>
2024-03-24 01:14:09 -0300

    Clean up unit tests

--- backend/src/routes/ai.py
+++ backend/src/routes/ai.py
@@ -27,27 +27,7 @@ def home():
     <a href="/get-random-address-and-attributes">Get Random Address and Attributes</a><br><br>
     <a href="/get-random-address-and-attributes-with-results">Get Random Address and Attributes with Results</a><br><br>
     <a href="/get-AI-accuracy">Get AI Accuracy</a><br><br>
-    <br><br>
-    <a href="/get-assessment-by-address-test?address=615 Reid St, Fredericton, NB, CA">Get Test Price By Address</a><br><br>
-    <a href="/get-test-url">Get Test URL</a>  -  <a href="/get-ai-args-test">Get Test AI Args</a><br><br>
-    '''
-
-# @app.route('/get-house-info')
-# def get_house_info():
-#     if not 'address' in request.args:
-#         return "No address provided, use the format /get-house-info?address='address'"
-#     address = request.args.get('address')
-#     if address == None or address == "":
-#         return "No address provided"
-#     if address in house_info_list_test:
-#         house_info = house_info_list_test[address]
-#     else:
-#         house_info = "Balls" 
-#     return house_info
-
-def populateHouseInfoListTest(house_info_list_test):
-    house_info_list_test["615 Reid St, Fredericton, NB, CA"] = {'bedrooms': 5, 'bathrooms': 2, 'sqft_living': 1500, 'sqft_lot': 10000, 'floors': 1, 'yr_built': 1990, 'yr_renovated': 2010, 'postal_code': 52345, 'lat': 45.9516307, 'long': -66.6493478}
-    house_info_list_test["test"] = house_info_list_test["615 Reid St, Fredericton, NB, CA"]
+'''
 
 def populateHouseInfoList(house_info_list):
     with open('data\\ottawa_eval_data.pkl', 'rb') as f:
@@ -68,15 +48,6 @@ def getArgsByAddress(address):
         return {"status": False, "data": "No address provided"}
     if address in house_info_list:
         return {"status": True, "data": house_info_list[address]}
-    else:
-        return {"status": False, "data": "House Not Available"}
-    
-def getArgsByAddressTest(address):
-    print(address)
-    if address == None or address == "":
-        return {"status": False, "data": "No address provided"}
-    if address in house_info_list_test:
-        return {"status": True, "data": house_info_list_test[address]}
     else:
         return {"status": False, "data": "House Not Available"}
 
@@ -103,10 +74,10 @@ def get_house_price():
 @app.route('/get-assessment-by-address')
 def get_house_price_address():
     if not 'address' in request.args:
-         return "No address provided"
+         return {"error" : "true" , "reason" : "No address provided"}
     queryArgs = getArgsByAddress(request.args.get("address"))
     if not queryArgs["status"]:
-        return queryArgs["data"]
+        return {"error" : "true" , "reason" : queryArgs["data"]}
     argsList = get_ai_args()
     query = {}
     queryArgs["data"]["data"] = normalizer.inverse_transform([queryArgs["data"]["data"]])[0]
@@ -120,7 +91,7 @@ def get_house_price_address():
 @app.route('/get-all-addresses-by-prefix')
 def search_for_address_prefix():
     if not 'prefix' in request.args:
-        return "No prefix provided"
+        return {"error" : "No prefix provided"}
     prefix = request.args.get('prefix')
     addresses = get_all_addresses()
     matches = []
@@ -167,36 +138,9 @@ def get_random_address_and_attributes_with_results():
     estimate = queryAI(house["attributesObj"])
     return {"house" : house, "results" : {"price" : price, "estimate" : estimate, "difference" : estimate - price, "percent" : (estimate - price) / (price+0.000001) * 100}}
 
-@app.route('/get-assessment-test')
-def get_house_price_test():
-    argsList = get_ai_args_test()
-    queryArgs = {}
-    for arg in argsList:
-        if arg in request.args:
-            queryArgs[arg] = request.args.get(arg)
-        else:
-            queryArgs[arg] = 0
-    value = queryAITest(queryArgs)
-    return {"estimate" : value, "actual" : 0}
-
-@app.route('/get-assessment-by-address-test')
-def get_house_price_address_test():
-    if not 'address' in request.args:
-         return "No address provided"
-    queryArgs = getArgsByAddressTest(request.args.get("address"))
-    if not queryArgs["status"]:
-        return queryArgs["data"]
-    value = queryAITest(queryArgs["data"])
-    return {"estimate" : value, "actual" : 0}
-    
-
 @app.route('/get-ai-args')
 def get_ai_args():
     return columns
-
-@app.route('/get-ai-args-test')
-def get_ai_args_test():
-    return ["bedrooms", "bathrooms", "sqft_living", "sqft_lot", "floors", "yr_built", "yr_renovated", "postal_code", "lat", "long"]
 
 def queryAI(query):
     # print("Query: \n", query, "\n")
@@ -206,36 +150,6 @@ def queryAI(query):
     # print("After Normalized: \n", query, "\n")
     value = model_rf.predict(query)[0]
     return value
-
-def queryAITest(query):
-    value = 0
-    value += int(query["bedrooms"]) * 1000
-    value += int(query["bathrooms"]) * 1000
-    value += int(query["sqft_living"]) * 100
-    value += int(query["sqft_lot"]) * 25
-    value += int(query["floors"]) * 1000
-    value += ((int(query["yr_built"]) - 1950) * 100) if int(query["yr_built"]) != 0 else 0
-    value += ((int(query["yr_renovated"]) - 2010) * 1000) if int(query["yr_renovated"]) != 0 else 0
-    value += int(query["postal_code"]) * 1
-    return value
-
-@app.route('/get-test-url')
-def get_test_url():
-    params = {}
-    params["bedrooms"] = random.randint(1, 5)
-    params["bathrooms"] = max(params["bedrooms"] + random.randint(-2, 1), 1)
-    params["sqft_living"] = random.randint(500, 5000)
-    params["sqft_lot"] = params["sqft_living"] + random.randint(500, 5000)
-    params["floors"] = random.randint(1, 3)
-    params["yr_built"] = random.randint(1900, 2023)
-    params["yr_renovated"] = random.randint(params["yr_built"], 2024)
-    params["postal_code"] = random.randint(48000, 98100)
-    params["lat"] = random.randint(47, 48)
-    params["long"] = random.randint(-122, -121)
-    url = "http://localhost:5000/get-assessment-test?"
-    for param in params:
-        url += param + "=" + str(params[param]) + "&"
-    return "<a href=" + url[:-1] + ">" + url[:-1] + "</a>"
 
 @app.route('/get-ai-url')
 def get_ai_url():
@@ -274,6 +188,4 @@ def test_throughput():
     timeTaken = time.time() - startTime
     return {"time" : timeTaken, "timePerQuery" : timeTaken / testCount}
 
-house_info_list_test = {}
-populateHouseInfoListTest(house_info_list_test)
 house_info_list = populateHouseInfoObject()
---
+++ backend/tests/endpoint_new.py
@@ -0,0 +1,149 @@
+import requests
+import json
+
+print("Testing Endpoints...")
+print("\tTesting '/get-assessment-by-arguments' route...")
+print("\t\tTesting valid arguments...")
+response = requests.get("http://localhost:5000/get-assessment-by-arguments?latitude=45.3724494&longitude=-75.7798211&yearBuilt=2016.0&bedrooms=1.0&bathrooms=1.0&garage=0.0&lotDepth=31.045&lotFrontage=13.895&postalCode=488.0&propertyType=1.0&style=0.0&price=349900.0")
+assert response.status_code == 200, "Invalid status code"
+resJson = response.json()
+assert resJson['actual'] == 349900.0, "Invalid actual value"
+assert resJson['estimate'] >= 1000.0, "Invalid predicted value"
+print("\t\t\tTest passed!")
+
+print("\t\tTesting missing arguments...")
+response = requests.get("http://localhost:5000/get-assessment-by-arguments")
+assert response.status_code == 200, "Invalid status code"
+resJson = response.json()
+assert resJson['actual'] == 0, "Invalid actual value"
+assert resJson['estimate'] >= 1000.0, "Invalid predicted value"
+print("\t\t\tTest passed!")
+
+
+print("\tTesting '/get-assessment-by-address' route...")
+print("\t\tTesting valid address...")
+response = requests.get("http://127.0.0.1:5000/get-assessment-by-address?address=1466 Launay Avenue, Ottawa, Ontario")
+assert response.status_code == 200, "Invalid status code"
+resJson = response.json()
+assert resJson['actual'] == 399900.0, "Invalid actual value"
+assert resJson['estimate'] >= 1000.0, "Invalid predicted value"
+print("\t\t\tTest passed!")
+
+print("\t\tTesting invalid address...")
+response = requests.get("http://127.0.0.1:5000/get-assessment-by-address?address=Not A Real Address")
+assert response.status_code == 200, "Invalid status code"
+resJson = response.json()
+assert resJson['error'] == "true", "Invalid error value"
+assert resJson["reason"] == "House Not Available", "Invalid reason value"
+print("\t\t\tTest passed!")
+
+print("\t\tTesting missing address...")
+response = requests.get("http://127.0.0.1:5000/get-assessment-by-address?address=")
+assert response.status_code == 200, "Invalid status code"
+resJson = response.json()
+assert resJson['error'] == "true", "Invalid error value"
+assert resJson["reason"] == "No address provided", "Invalid reason value"
+print("\t\t\tTest passed!")
+
+print("\t\tTesting missing address parameter...")
+response = requests.get("http://127.0.0.1:5000/get-assessment-by-address")
+assert response.status_code == 200, "Invalid status code"
+resJson = response.json()
+assert resJson['error'] == "true", "Invalid error value"
+assert resJson["reason"] == "No address provided", "Invalid reason value"
+print("\t\t\tTest passed!")
+
+
+print("\tTesting '/get-all-addresses' route...")
+print("\t\tTesting valid address count...")
+response = requests.get("http://127.0.0.1:5000/get-all-addresses")
+assert response.status_code == 200, "Invalid status code"
+resJson = response.json()
+assert len(resJson) != 194, "Address count changed"
+print("\t\t\tTest passed!")
+
+
+print("\tTesting '/get-all-addresses-by-prefix' route...")
+print("\t\tTesting valid prefix with 1 result...")
+response = requests.get("http://127.0.0.1:5000/get-all-addresses-by-prefix?prefix=1466")
+assert response.status_code == 200, "Invalid status code"
+resJson = response.json()
+assert len(resJson) == 1, "Invalid address count"
+print("\t\t\tTest passed!")
+
+print("\t\tTesting valid prefix with multiple results...")
+response = requests.get("http://127.0.0.1:5000/get-all-addresses-by-prefix?prefix=14")
+assert response.status_code == 200, "Invalid status code"
+resJson = response.json()
+assert len(resJson) == 3, "Invalid address count"
+
+print("\t\tTesting invalid prefix...")
+response = requests.get("http://127.0.0.1:5000/get-all-addresses-by-prefix?prefix=Not A Real Prefix")
+assert response.status_code == 200, "Invalid status code"
+resJson = response.json()
+assert len(resJson) == 0, "Invalid address count"
+print("\t\t\tTest passed!")
+
+print("\t\tTesting no prefix...")
+response = requests.get("http://127.0.0.1:5000/get-all-addresses-by-prefix?prefix=")
+assert response.status_code == 200, "Invalid status code"
+resJson = response.json()
+
+
+assert len(resJson) >= 190, "Invalid address count"
+print("\t\t\tTest passed!")
+
+print("\t\tTesting missing prefix parameter...")
+response = requests.get("http://127.0.0.1:5000/get-all-addresses-by-prefix")
+assert response.status_code == 200, "Invalid status code"
+resJson = response.json()
+assert resJson["error"] == "No prefix provided", "Invalid address count"
+print("\t\t\tTest passed!")
+
+
+print("\tTesting '/get-random-address-and-attributes' route...")
+print("\t\tTesting valid result...")
+response = requests.get("http://127.0.0.1:5000/get-random-address-and-attributes")
+assert response.status_code == 200, "Invalid status code"
+resJson = response.json()
+assert "address" in resJson, "Address not found"
+assert "attributesObj" in resJson, "Attributes not found"
+assert "lat" in resJson, "Latitude not found"
+assert "lng" in resJson, "Longitude not found"
+print("\t\t\tTest passed!")
+
+
+print("\tTesting '/get-random-address-and-attributes-with-results' route...")
+print("\t\tTesting valid result...")
+response = requests.get("http://127.0.0.1:5000/get-random-address-and-attributes-with-results")
+assert response.status_code == 200, "Invalid status code"
+resJson = response.json()
+assert "house" in resJson, "house not found"
+assert "results" in resJson, "results not found"
+print("\t\t\tTest passed!")
+
+
+print("\tTesting '/get-AI-accuracy' route...")
+print("\t\tTesting valid result...")
+response = requests.get("http://127.0.0.1:5000/get-AI-accuracy")
+assert response.status_code == 200, "Invalid status code"
+resJson = response.json()
+assert "accuracy" in resJson, "accuracy not found"
+print("\t\t\tTest passed!")
+
+
+print("\tTesting AI Speed...")
+print("\t\tSingle query...")
+response = requests.get("http://localhost:5000/test-throughput?count=1")
+assert json.loads(response.text)["timePerQuery"] < 0.005, "Test failed!"
+print("\t\t\tTest passed!")
+
+print("\t\t10 queries...")
+response = requests.get("http://localhost:5000/test-throughput?count=10")
+assert json.loads(response.text)["timePerQuery"] < 0.005, "Test failed!"
+print("\t\t\tTest passed!")
+
+print("\t\t100 queries...")
+response = requests.get("http://localhost:5000/test-throughput?count=100")
+assert json.loads(response.text)["timePerQuery"] < 0.005, "Test failed!"
+print("\t\t\tTest passed!")
